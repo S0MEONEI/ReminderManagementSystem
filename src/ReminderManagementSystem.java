82bc2439dac31f726012185507ff712352da3ed6
S0MEONEI <2021011344@gnu.ac.kr>
2022-03-14 11:14:13 +0900

    ReminderManagementSystem

---
+++ src/ReminderManagementSystem.java
@@ -0,0 +1,12 @@
+
+public class ReminderManagementSystem {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Reminder");
+		System.out.println("2. Delete Reminder");
+		System.out.println("3. Edit Reminder");
+		System.out.println("4. View Reminder");
+		System.out.println("5. Exit");
+	}
+	
+}
