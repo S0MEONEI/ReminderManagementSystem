df7377c8a6bec9adf1d3227b27c17ccbc4beaa99
S0MEONEI <2021011344@gnu.ac.kr>
2022-04-04 19:50:47 +0900

    배열추가

--- src/ReminderManagementSystem.java
+++ src/ReminderManagementSystem.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class ReminderManagementSystem {
 
 	static Scanner input = new Scanner(System.in);
-	static double deadline = 0;
-	static String wyd = null;
+	static double[] deadline = new double [10];
+	static String[] wyd = new String [10];
+	static int step = 0;
 	
 	public static void main(String[] args) {
 		int num = 0;
@@ -40,28 +41,36 @@ public class ReminderManagementSystem {
 }
 	public static void addReminder() {
 		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a step to want: ");
+		step = input.nextInt();
 		System.out.print("Deadline(mm.dd):");
-		deadline = input.nextDouble();
+		deadline[step] = input.nextDouble();
 		input.nextLine();
 		System.out.print("What you do?:");
-		wyd = input.nextLine();
+		wyd[step] = input.next();
 	}
 	
 	public static void deleteReminder() {
-		deadline = 0;
-		wyd = null;
+		System.out.println("Enter a step to delete");
+		step = input.nextInt();
+		deadline[step] = 0;
+		wyd[step] = null;
 	}
 	
 	public static void editReminder() {
-		System.out.print("Deadline:");
-		deadline = input.nextDouble();
+		System.out.println("Enter a step to want: ");
+		step = input.nextInt();
+		System.out.print("Deadline(mm.dd):");
+		deadline[step] = input.nextDouble();
 		input.nextLine();
 		System.out.print("What you do?:");
-		wyd = input.nextLine();
-	}
+		wyd[step] = input.next();	}
 	
 	public static void viewReminder() {
-		System.out.println("Deadline: " + deadline);
-		System.out.println("What you do?: " + wyd);
+		for(step=0; step<10; step++) {
+			System.out.println("Deadline: " + deadline[step]);
+			System.out.println("What you do?: " + wyd[step]);
+			System.out.println();
+		}
 	} 
 }
