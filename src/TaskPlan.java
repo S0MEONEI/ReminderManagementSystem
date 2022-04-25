22631e4a8ee101dad83eec8965b984834cbd4de9
S0MEONEI <2021011344@gnu.ac.kr>
2022-04-25 21:34:02 +0900

    Plan의 자식

---
+++ src/TaskPlan.java
@@ -0,0 +1,19 @@
+
+public class TaskPlan extends Plan {
+	
+	private String subject;
+	
+	TaskPlan() {
+		
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+}
