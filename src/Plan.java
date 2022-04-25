6c9bba5c07426f7ae094e676475b87afe9773461
S0MEONEI <2021011344@gnu.ac.kr>
2022-04-25 21:34:59 +0900

    겟셋

---
+++ src/Plan.java
@@ -0,0 +1,35 @@
+public class Plan {
+	
+	private String deadline;
+	private String wyd;
+	private String name;
+	
+	Plan() {
+		
+	}
+	
+	public String getDeadline() {
+		return deadline;
+	}
+	
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+	
+	public String getWyd() {
+		return wyd;
+	}
+	
+	public void setWyd(String wyd) {
+		this.wyd = wyd;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
