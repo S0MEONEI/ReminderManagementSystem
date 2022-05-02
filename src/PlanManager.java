154a3f00fe8ce53414ba453a9d81c07c21eb2b01
S0MEONEI <2021011344@gnu.ac.kr>
2022-05-02 18:44:00 +0900

    6

--- src/PlanManager.java
+++ src/PlanManager.java
@@ -3,6 +3,7 @@ public class PlanManager {
 	Scanner input = new Scanner(System.in);
 	TaskPlan[] tp = new TaskPlan[10];
 	int idx;
+	
 	public void arrayInit()
 	{
 		for(int i = 0; i < 10; i++) {
@@ -11,30 +12,32 @@ public class PlanManager {
 		}
 	}
 	public void addReminder() {
-		System.out.print("What is plan name?:");
-		tp[idx].setName(input.nextLine());
+		System.out.print("What is plan name?: ");
+		tp[idx].setSubject(input.nextLine());
 		input.nextLine();
-		System.out.print("Deadline(mm.dd):");
+		System.out.print("Deadline(mm.dd): ");
 		tp[idx].setDeadline(input.nextLine());
 		input.nextLine();
-		System.out.print("What will you do?:");
+		System.out.print("What will you do?: ");
 		tp[idx].setWyd(input.nextLine());
 		input.nextLine();
 		this.idx++;
 	}
+	
 	public void deleteReminder() {
 		System.out.println("Enter an index to delete");
 		input.nextLine();
 		int i = input.nextInt();
-		tp[i].setName(null);
+		tp[i].setSubject(null);
 		tp[i].setDeadline(null);
 		tp[i].setWyd(null);
 	}
+	
 	public void editReminder() {
 		System.out.println("Enter an index to edit: ");
 		int i = input.nextInt();
 		System.out.print("What is plan name?:");
-		tp[idx].setName(input.nextLine());
+		tp[idx].setSubject(input.nextLine());
 		input.nextLine();
 		System.out.print("edit Deadline(mm.dd):");
 		tp[i].setDeadline(input.nextLine());
@@ -44,17 +47,10 @@ public class PlanManager {
 		input.nextLine();
 	
 	}
+	
 	public void viewReminder() {
 		for(int i = 0; i < 10; i++) {
-				System.out.println("What is plan name?:" + tp[i].getName());
-				input.nextLine();
-				System.out.println("Deadline: " + tp[i].getDeadline());
-				input.nextLine();
-				System.out.println("What you do?: " + tp[i].getWyd());
-				input.nextLine();
-				System.out.println();
-				input.nextLine();
-			
+			tp[i].printInfo();
 		}
 	}
 }
