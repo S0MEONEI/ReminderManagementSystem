e743f323c247aeee227b1124530137f8950361f4
S0MEONEI <2021011344@gnu.ac.kr>
2022-05-16 11:19:00 +0900

    Assignment #8

--- src/ReminderManagementSystem.java
+++ src/ReminderManagementSystem.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReminderManagementSystem {
@@ -19,36 +20,47 @@ public class ReminderManagementSystem {
 		ManagementTopic(Topic);
 		ManagementTopic("03", "14");
 		
+		ReminderManagementSystem rm = new ReminderManagementSystem();
 		PlanManager pm = new PlanManager();
 		pm.arrayInit();
+		try {
+			while (num != 6) {
+				System.out.println("---Reminder Management System Menu---");
+				System.out.println("1. Add Reminder");
+				System.out.println("2. Delete Reminder");
+				System.out.println("3. Edit Reminder");
+				System.out.println("4. View Reminder");
+				System.out.println("5. Show a menu");
+				System.out.println("6. Exit");
+				System.out.println("1부터 6 중 번호를 선택하세요.");
 		
-		while (num != 6) {
-			System.out.println("---Reminder Management System Menu---");
-			System.out.println("1. Add Reminder");
-			System.out.println("2. Delete Reminder");
-			System.out.println("3. Edit Reminder");
-			System.out.println("4. View Reminder");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.println("1부터 6 중 번호를 선택하세요.");
-		
-			num = input.nextInt();
+				num = input.nextInt();
 			
-			if (num == 1) {
-				pm.addReminder();
+				if (num == 1) {
+					pm.addReminder();
+				}
+				else if (num == 2) {
+					pm.deleteReminder();
+				}
+				else if (num == 3) {
+					pm.editReminder();
+				}
+				else if (num == 4) {
+					pm.viewReminder();
+				}
+				else {
+					continue;
+				}
 			}
-			else if (num == 2) {
-				pm.deleteReminder();
+		
 			}
-			else if (num == 3) {
-				pm.editReminder();
-			}
-			else if (num == 4) {
-				pm.viewReminder();
-			}
-			else {
-				continue;
-			}
+		catch(InputMismatchException e){
+			System.out.println("InputMismatchException");
+		}
+		catch(NullPointerException e){
+			System.out.println("NullPointerException");
+
 		}
 	}
 }
+	
