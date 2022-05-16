cd1b144a4c9b8e9ef95f8f065487eeb1b1404cc0
S0MEONEI <2021011344@gnu.ac.kr>
2022-05-16 11:19:45 +0900

    Assignment #8

--- src/TaskPlan.java
+++ src/TaskPlan.java
@@ -3,6 +3,7 @@ public class TaskPlan extends Plan implements subject {
 	
 	private String subject;
 
+
 	public String getSubject() {
 		return subject;
 	}
@@ -13,9 +14,12 @@ public class TaskPlan extends Plan implements subject {
 	
 	public void printInfo()
 	{
-		System.out.println("Deadline: " + getDeadline());
-		System.out.println("Subject: " + getSubject());
-		System.out.println("Plan: " + getWyd());
+				System.out.println("Deadline: " + getDeadline());
+				System.out.println("Subject: " + getSubject());
+				System.out.println("Plan: " + getWyd());
+				System.out.println();
+			
+		
 	}
 }
 
